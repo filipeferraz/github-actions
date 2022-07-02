8191ad73a5bd5b654492fe8b3373b0945530543d
Filipe Ferraz <filipe.ferraz@trt20.jus.br>
2022-07-02 00:19:01 -0300

    fix: test ci

--- react-app/release.config.js
+++ react-app/release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/filipeferraz/github-actions",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
-    ]
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/filipeferraz/github-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
